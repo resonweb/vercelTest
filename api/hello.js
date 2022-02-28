4e673633d79dc8090a422c508cb51735933ca713
resonhou <resonhou@163.com>
2022-02-28 22:19:38 +0800

    修改

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,24 @@
+// var util = require("util")
+// var url = require('url')
+
 export default function handler(request, response) {
-    response.status(200).json({
-      name: "Hello world"
-    });
+    var message = 'Hello world!'
+
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(message);
+    if(request.query && request.query.name){
+        var name = request.query.name;
+        response.write("name: " + name);
+    };
+    if (request.body){
+        var body = request.body
+        response.write("body: " + body);
+    };
+    
+    response.end();
+
+    // response.status(200).json(query);
+    // response.json()
+    // response.send("Everything is ok! 请求文件： " + request.url);
   }
